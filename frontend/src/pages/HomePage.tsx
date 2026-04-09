8df4402af47028acd661b7ffe26dde209428a4ea
Pathum-Piyumal <22cse0388@ms.sab.ac.lk>
2026-04-09 23:31:42 +0530

    feat:implement the featuresSection inthe home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '../../components/home/HeroSection'
 import StatsBar from '../../components/home/StatsBar'
+import FeaturesSection from '../../components/home/FeaturesSection'
 
 export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen bg-brand-dark">
       <HeroSection />
       <StatsBar />
+      <FeaturesSection />
     </div>
   )
 }
