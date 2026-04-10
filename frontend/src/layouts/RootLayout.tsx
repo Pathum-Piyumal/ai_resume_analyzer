80c37f2bbe37ac77937a42884ba62fc9bd059cf2
Pathum-Piyumal <22cse0388@ms.sab.ac.lk>
2026-04-10 23:48:51 +0530

    feat:implement the footer

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
 
 interface RootLayoutProps {
   children: React.ReactNode
@@ -8,11 +9,11 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <div className="min-h-screen bg-brand-dark text-brand-textPrimary flex flex-col selection:bg-brand-orange/30 selection:text-white">
-      <Navbar /> {/* <-- 2. Render the Navbar */}
+      <Navbar />
       <main className="flex-grow">
         {children}
       </main>
-      {/* Footer will go here */}
+      <Footer />
     </div>
   )
 }
